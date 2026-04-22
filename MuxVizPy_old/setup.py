52907844e9190478257bb42b8ab30e3f50eee90c
francescozambelli <francesco.zambelli98@gmail.com>
2026-04-22 11:20:07 +0200

    removing muxvizpy_old

--- MuxVizPy_old/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setupsetup(
-    name='MuxVizPy',
-    packages=find_packages(include=["MuxVizPy","build", "versatility", "topology"]),
-    version='0.1.0',
-    description='My first Python library',
-    author='Francesco',
-    license='None',
-)
